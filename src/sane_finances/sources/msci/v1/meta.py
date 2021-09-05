44be6bdcf1b90ad745ab63cd3ab5e1cfdd5489b1
Stanley Sane <stanleysane@gmail.com>
2021-09-05 14:40:57 +0300

    Fix pylint 3.8

--- src/sane_finances/sources/msci/v1/meta.py
+++ src/sane_finances/sources/msci/v1/meta.py
@@ -19,7 +19,7 @@ from ....annotations import Volatile, LEGACY_ANNOTATIONS, Description
 if LEGACY_ANNOTATIONS:  # pragma: no cover
     from ....annotations import Annotated
 else:  # pragma: no cover
-    from typing import Annotated
+    from typing import Annotated  # pylint: disable=no-name-in-module
 
 
 class Scopes(enum.Enum):
