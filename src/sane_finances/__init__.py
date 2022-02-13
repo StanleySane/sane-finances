180954bf1535e56f1ceceed32c5f1a0a22523c37
Stanley Sane <stanleysane@gmail.com>
2022-02-13 14:15:22 +0300

    Next dev version

--- src/sane_finances/__init__.py
+++ src/sane_finances/__init__.py
@@ -5,4 +5,4 @@ __author__ = "Stanley Sane"
 __copyright__ = "Copyright (c) 2018-2022, Sane Development"
 __license__ = "The BSD 3-Clause License"
 
-__version__ = '1.0.1.dev6'
+__version__ = '1.0.1.dev7'
